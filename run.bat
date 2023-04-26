24c7efdf783b020c323dec1f72e708b9f0293f41
Sandz0814 <sandrojimena0814@gmail.com>
2023-04-26 23:02:30 +0800

    5th commit......

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 rem allure serve C:\Users\Sandz\PycharmProjects\HybridFramework\allure_reports
 rem pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ -- browser chrome
-pytest -s -v -m "sanity and regression" --html=./Reports/report.html testCases/ -- browser chrome
+pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
