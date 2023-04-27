c60bc18f3d801323d37eee9d0a868cd21d65eae1
Sandz0814 <sandrojimena0814@gmail.com>
2023-04-27 12:04:02 +0800

    11th commit.......

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-pytess -s -v -m "sanity and regression" --html=./Reports/report.html testCases/ -- browser chrome
+pytest -s -v -m "sanity and regression" --html=./Reports/report.html testCases/ -- browser chrome
